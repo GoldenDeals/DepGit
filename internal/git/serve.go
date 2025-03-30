8a627f4e18aaff60d821af0a4a947918edf96234
Ivan Nebotov <goldendeals@yandex.ru>
2025-03-30 23:24:48 +0300

    chore: Fuckit

--- internal/git/serve.go
+++ internal/git/serve.go
@@ -1,8 +1,14 @@
 package git
 
-type Storage interface{}
+import "io"
+
+type Storage interface {
+	Put(namespace string, objname string, obj io.Reader) error
+	Get(namespace string, objname string) (io.Reader, error)
+	List(namespace string) ([]string, error)
+}
 
 type Server struct {
 	config  Config
-	storage Storage
+	stroage Storage
 }
