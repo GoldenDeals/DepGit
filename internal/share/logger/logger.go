a1366e847ca2364055c8263a30450f6ba316a419
Ivan Nebotov <goldendeals@yandex.ru>
2025-03-30 23:56:29 +0300

    feat: added logger and errors

---
+++ internal/share/logger/logger.go
@@ -0,0 +1,33 @@
+package logger
+
+import (
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type customLogger struct {
+	defaultField string
+	formatter    logrus.Formatter
+}
+
+func (l customLogger) Format(entry *logrus.Entry) ([]byte, error) {
+	entry.Data["src"] = l.defaultField
+	entry = entry.WithTime(time.Now())
+	return l.formatter.Format(entry)
+}
+
+func New(name string, logLevel ...logrus.Level) *logrus.Logger {
+	log := logrus.New()
+	log.SetFormatter(customLogger{
+		defaultField: name,
+		formatter:    logrus.StandardLogger().Formatter,
+	})
+	if len(logLevel) > 0 {
+		log.SetLevel(logLevel[0])
+	} else {
+		log.SetLevel(logrus.DebugLevel)
+	}
+
+	return log
+}
