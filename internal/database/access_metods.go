99efca0f83752a7ad14249fe5e487a54ef094089
Lacky1234union <rogachevegorushka@yandex.ru>
2025-04-03 18:38:49 +0300

    zbs

--- internal/database/access_metods.go
+++ internal/database/access_metods.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"database/sql"
-	"log"
 	"time"
 
 	"github.com/gobwas/glob"
@@ -10,7 +9,21 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type IDT = uuid.UUID
+const (
+	SSH_KEY_TYPE_RSA    SSH_KEY_TYPE = iota + 1
+	RSA_SHA2_256        SSH_KEY_TYPE
+	RSA_SHA2_512        SSH_KEY_TYPE
+	SSH_RSA             SSH_KEY_TYPE
+	ECDSA_SHA2_NISTP256 SSH_KEY_TYPE
+	// .... TODO: Добавить еще
+)
+
+var log = logger.New("db")
+
+type (
+	IDT          = uuid.UUID
+	SSH_KEY_TYPE int
+)
 
 type User struct {
 	ID uuid.UUID
@@ -22,17 +35,6 @@ type User struct {
 	Edited  time.Time
 	Deleted time.Time
 }
-
-type SSH_KEY_TYPE int
-
-const (
-	SSH_KEY_TYPE_RSA    SSH_KEY_TYPE = iota + 1
-	RSA_SHA2_256        SSH_KEY_TYPE
-	RSA_SHA2_512        SSH_KEY_TYPE
-	SSH_RSA             SSH_KEY_TYPE
-	ECDSA_SHA2_NISTP256 SSH_KEY_TYPE
-	// .... TODO: Добавить еще
-)
 
 type SshKey struct {
 	ID     uuid.UUID
@@ -52,6 +54,7 @@ type Repo struct {
 	Name string
 
 	Created time.Time
+	Edited  time.Time
 	Deleted time.Time
 }
 
@@ -76,62 +79,83 @@ type DB struct {
 	db *sql.DB
 }
 
-func (d *DB) Init() (err error) {
+func (d *DB) Init() error {
+	var err error
 	d.db, err = sql.Open("sqlite3", "./migrations/access.db")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error init Database sql open ", err)
 		return err
 	}
 	err = d.db.Ping()
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error init Database ping ", err)
 		return err
 	}
 	logrus.Debug("Database access.db open")
 	return nil
 }
 
-func (d *DB) CreateUser(user *User) (err error) {
+func (d *DB) CreateUser(user *User) error {
 	statement, err := d.db.Prepare("INSERT INTO users (id, name, email, created, edited, deleted) VALUES (?,?,?,?,?)")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error Create User ", user.ID, err)
 		return err
 	}
-	statement.Exec(user.ID.String(), user.Name, user.Created, user.Edited, user.Deleted)
-	logrus.Trace("Create User", user)
+	userid := uuid.New().String()
+	_, err = statement.Exec(userid, user.Name, time.Now().Format(time.DateTime), user.Edited.Format(time.DateTime), user.Deleted.Format(time.DateTime))
+	if err != nil {
+		log.Errorf("Error Execute sql request ", err)
+		return err
+	}
+	logrus.Trace("Create User", userid, user.Name)
 	return nil
 }
 
 func (d *DB) EditUser(userid IDT, user *User) error {
 	statement, err := d.db.Prepare("UPDATE users SET name = ? , email = ? , created = ?, edited = ?, deleted = ? WHERE id = ?")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error edit user ", user.ID, err)
 		return err
 	}
-	statement.Exec(user.Name, user.Email, user.Created, user.Edited, user.Deleted, userid.String())
-	logrus.Trace("Edit user", user)
+	_, err = statement.Exec(user.Name, user.Email, user.Created.Format(time.DateTime), time.Now().Format(time.DateTime), user.Deleted.Format(time.DateTime), userid.String())
+	if err != nil {
+		log.Errorf("Error Execute sql request ", err)
+		return err
+	}
+	logrus.Trace("Edit user", user.ID, user.Name)
 	return nil
 }
 
 func (d *DB) DeleteUser(userid IDT) error {
 	statement, err := d.db.Prepare("DELETE FROM users WHERE id = ?")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error delete User ", userid, err)
 		return err
 	}
-	statement.Exec(userid)
+	_, err = statement.Exec(userid)
+	if err != nil {
+		log.Errorf("Error Execute sql request ", err)
+		return err
+	}
 	logrus.Trace("Delete user", userid)
 	return nil
 }
 
 func (d *DB) GetUser(userid IDT) (User, error) {
 	var user User
-	row := d.db.QueryRow("SELECT * FORM users WHERE id = ?", userid)
+	var err error
 	var identif string
-	row.Scan(&identif, &user.Name, &user.Email, &user.Created, &user.Edited, &user.Deleted)
-	user.ID, err = uuid.FromString(identif)
+	var id IDT
+	row := d.db.QueryRow("SELECT * FORM users WHERE id = ?", userid)
+	err = row.Scan(&identif, &user.Name, &user.Email, &user.Created, &user.Edited, &user.Deleted)
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error Scan sql request ", err)
+		return user, err
+	}
+	err = id.UnmarshalText([]byte(identif))
+	user.ID = id
+	if err != nil {
+		log.Errorf("Error Get user", userid, err)
 		return user, err
 	}
 	logrus.Trace("get info about user", user)
@@ -142,44 +166,55 @@ func (d *DB) GetUsers() ([]User, error) {
 	users := make([]User, 0, 16)
 	row, err := d.db.Query("SELECT * FORM users ")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error GetUsers ", err)
 		return users, err
 	}
 	for row.Next() {
 		var user User
 		var identif string
+		var id IDT
 		row.Scan(&user.ID, &user.Name, &user.Email, &user.Created, &user.Edited, &user.Deleted)
-		user.ID, err = uuid.FromString(identif)
+		err = id.UnmarshalText([]byte(identif))
+		user.ID = id
 		if err != nil {
-			log.Errorf("Error %s", err)
+			log.Errorf("Error scan GetUsers", identif, err)
 			return users, err
 		}
 		users = append(users, user)
-		logrus.Trace("get info about user", user)
+		logrus.Trace("get info about user", user.ID, user.Name)
 	}
 
 	return users, nil
 }
 
 func (d *DB) AddSshKey(userid IDT, key *SshKey) error {
-	statement, err := d.db.Prepare("INSERT INTO keys (id,user_id, name, type, data,  created, deleted) VALUES (?,?,?,?,?,?)")
+	var err error
+	statement, err := d.db.Prepare("INSERT INTO keys (id,user_id, name, type, key,  created, deleted) VALUES (?,?,?,?,?,?)")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error AddSshKey ", userid, key, err)
 		return err
 	}
-	statement.Exec(key.ID.String(), key.UserID.String(), key.Name, key.Type, key.Data, key.Created, key.Deleted)
-	logrus.Trace("Add SSH KEY", key, " ", userid)
+	_, err = statement.Exec(key.ID.String(), key.UserID.String(), key.Name, key.Type, key.Data, time.Now().Format(time.DateTime), key.Deleted.Format(time.DateTime))
+	if err != nil {
+		log.Errorf("Error Execute sql request ", err) //
+		return err
+	}
+	logrus.Trace("Add SSH KEY", key, userid)
 	return nil
 }
 
 func (d *DB) DelSshKey(userid IDT, keyid IDT) error {
 	statement, err := d.db.Prepare("DELETE * FORM keys WHERE id = ?, AND WHERE user_id = ?")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error DelSshKey ", userid, keyid, err)
 		return err
 	}
-	statement.Exec(keyid, userid)
-	logrus.Trace("Delete SSH key ", userid, " ", keyid)
+	_, err = statement.Exec(keyid, userid)
+	if err != nil {
+		log.Errorf("Error Execute sql request ", err) //
+		return err
+	}
+	logrus.Trace("Delete SSH key ", userid, keyid)
 	return nil
 }
 
@@ -187,12 +222,16 @@ func (d *DB) GetSshKeys(userid IDT) ([]SshKey, error) {
 	keys := make([]SshKey, 0, 16)
 	row, err := d.db.Query("SELECT * FROM keys WHERE user_id = ?", userid)
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error GetSshKeys ", userid, err)
 		return keys, err
 	}
 	for row.Next() {
 		var key SshKey
-		row.Scan(&key.ID, &key.UserID, &key.Name, &key.Type, &key.Data, &key.Created, &key.Deleted)
+		err = row.Scan(&key.ID, &key.UserID, &key.Name, &key.Type, &key.Data, &key.Created, &key.Deleted)
+		if err != nil {
+			log.Errorf("Error Scan sql request ", err) //
+			return keys, err
+		}
 		keys = append(keys, key)
 	}
 	logrus.Trace("Get SSH keys user ", userid)
@@ -200,35 +239,48 @@ func (d *DB) GetSshKeys(userid IDT) ([]SshKey, error) {
 }
 
 func (d *DB) CreateRepo(repo *Repo) error {
-	statement, err := d.db.Prepare("INSERT INTO permitions (id, name, created, deleted) VALUES (?, ?, ?, ?)")
+	statement, err := d.db.Prepare("INSERT INTO permitions (id, name, created, edited ,deleted) VALUES (?, ?,?, ?, ?)")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error Create Repo ", repo.ID, err)
 		return err
 	}
-	statement.Exec(repo.ID, repo.Name, repo.Created, repo.Deleted)
-	logrus.Trace("Create Repositor", repo)
+	repoid := uuid.New().String()
+	_, err = statement.Exec(repoid, repo.Name, time.Now().Format(time.DateTime), repo.Edited, repo.Deleted.Format(time.DateTime))
+	if err != nil {
+		log.Errorf("Error Execute sql request ", err) //
+		return err
+	}
+	logrus.Trace("Create Repositor", repo.ID)
 	return nil
 }
 
 func (d *DB) DeleteRepo(repoid IDT) error {
 	statement, err := d.db.Prepare("DELETE * FORM permitions WHERE id = ?")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error DeleteRepo ", repoid, err)
 		return err
 	}
-	statement.Exec(repoid)
-	logrus.Trace("Deleted Repositor", repoid)
+	_, err = statement.Exec(repoid)
+	if err != nil {
+		log.Errorf("Error Execute sql request ", err) //
+		return err
+	}
+	logrus.Trace("Deleted.Format(time.DateTime) Repositor", repoid)
 	return nil
 }
 
 func (d *DB) UpdateRepo(repoid IDT, repo Repo) error {
 	statement, err := d.db.Prepare("UPDATE permitions SET name = ? , created = ?, deleted = ? WHERE id = ?")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error UpdateRepo ", repoid, err)
 		return err
 	}
-	statement.Exec(repo.Name, repo.Created, repo.Deleted, repo.ID.String())
-	logrus.Trace("Edit Repositor ", repo)
+	_, err = statement.Exec(repo.Name, repo.Created.Format(time.DateTime), repo.Deleted.Format(time.DateTime), repoid.String())
+	if err != nil {
+		log.Errorf("Error Execute sql request ", err) //
+		return err
+	}
+	logrus.Trace("Edit Repositor ", repo.ID)
 	return nil
 }
 
@@ -236,17 +288,23 @@ func (d *DB) GetRepo(repoid IDT) (Repo, error) {
 	var repo Repo
 	row, err := d.db.Query("SELECT * FORM permitions WHERE id = ?", repoid)
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error GetRepo ", repoid, err)
 		return repo, err
 	}
 	var identif string
-	row.Scan(&identif, &repo.Name, &repo.Created, &repo.Deleted)
-	repo.ID, err = uuid.FromString(identif)
+	var id IDT
+	err = row.Scan(&identif, &repo.Name, &repo.Created, &repo.Edited, &repo.Deleted)
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error scan sql request ", err) //
 		return repo, err
 	}
-	logrus.Trace("get info about Repositor ", repo)
+	err = id.UnmarshalText([]byte(identif))
+	repo.ID = id
+	if err != nil {
+		log.Errorf("Error GetRepo scan", identif, repoid, err)
+		return repo, err
+	}
+	logrus.Trace("get info about Repositor ", repo.ID)
 	return repo, nil
 }
 
@@ -255,20 +313,26 @@ func (d *DB) GetRepos() ([]Repo, error) {
 	repos := make([]Repo, 0, 16)
 	row, err := d.db.Query("SELECT * FORM permitions ")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error GetRepos ", err)
 		return repos, err
 	}
 	for row.Next() {
 		var repo Repo
 		var identif string
-		row.Scan(&identif, &repo.Name, &repo.Created, &repo.Deleted)
-		repo.ID, err = uuid.FromString(identif)
+		var id IDT
+		err = row.Scan(&identif, &repo.Name, &repo.Created, &repo.Deleted)
 		if err != nil {
-			log.Errorf("Error %s", err)
+			log.Errorf("Error scan sql request ", err) //
+			return repos, err
+		}
+		err = id.UnmarshalText([]byte(identif))
+		repo.ID = id
+		if err != nil {
+			log.Errorf("Error GetRepos scan ", identif, err)
 			return repos, err
 		}
 		repos = append(repos, repo)
-		logrus.Trace("get info about Repositor", repo)
+		logrus.Trace("get info about Repositor", repo.ID)
 	}
 
 	return repos, nil
@@ -277,10 +341,15 @@ func (d *DB) GetRepos() ([]Repo, error) {
 func (d *DB) CreateAccessRole(ar *AccessRole) error {
 	statement, err := d.db.Prepare("INSERT INTO roles (role_id ,user_id, rep_id , branch, created, deleted) VALUES (?,?, ?,?, ?, ?)")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error CreateAccessRole ", ar.RepoID, ar.RepoID, ar.UserID, err)
 		return err
 	}
-	statement.Exec(ar.RoleID, ar.UserID, ar.RepoID, ar.Branches, ar.Created, ar.Deleted)
+	roleid := uuid.New().String()
+	_, err = statement.Exec(roleid, ar.UserID, ar.RepoID, ar.Branches, ar.Created.Format(time.DateTime), ar.Deleted.Format(time.DateTime))
+	if err != nil {
+		log.Errorf("Error Execute sql request ", err) //
+		return err
+	}
 	logrus.Trace("Create Repositor", ar)
 	return nil
 }
@@ -288,39 +357,57 @@ func (d *DB) CreateAccessRole(ar *AccessRole) error {
 func (d *DB) EditAccessRole(roleid IDT, ar *AccessRole) error {
 	statement, err := d.db.Prepare("UPDATE roles SET user_id = ?, rep_id = ?, branch = ?, created = ?, deleted = ? WHERE role_id = ?")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error EditAccessRole ", roleid, err)
 		return err
 	}
-	statement.Exec(ar.UserID.String(), ar.RepoID.String(), ar.Branches, ar.Created, ar.Deleted, ar.RoleID.String())
-	logrus.Trace("Edit role", ar)
+	_, err = statement.Exec(ar.UserID.String(), ar.RepoID.String(), ar.Branches, ar.Created.Format(time.DateTime), ar.Deleted.Format(time.DateTime), ar.RoleID.String())
+	if err != nil {
+		log.Errorf("Error Execute sql request ", err) //
+		return err
+	}
+	logrus.Trace("Edit role", ar.RoleID)
 	return nil
 }
 
 func (d *DB) DeleteAccessRole(roleid IDT) error {
 	statement, err := d.db.Prepare("DELETE * FORM roles WHERE role_id = ?")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error DeleteAccessRole ", roleid, err)
 		return err
 	}
-	statement.Exec(roleid)
-	logrus.Trace("Deleted Role", roleid)
+	_, err = statement.Exec(roleid)
+	if err != nil {
+		log.Errorf("Error Execute sql request ", err) //
+		return err
+	}
+	logrus.Trace("Deleted.Format(time.DateTime) Role", roleid)
 	return nil
 }
 
 func (d *DB) GetAccessRole(roleid IDT) (AccessRole, error) {
 	var role AccessRole
-	row, err := d.db.Query("SELECT * FORM roles WHERE id = ?", roleid)
+	var err error
+	row := d.db.QueryRow("SELECT * FORM roles WHERE id = ?", roleid)
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error GetAccessRoles ", roleid, err)
 		return role, err
 	}
+	var idrole, iduser, idrepo IDT
 	var identifRole, identifUser, identifReposit string
 	row.Scan(&identifRole, &identifUser, &identifReposit, &role.Branches, &role.Created, &role.Deleted)
-	role.RoleID, err = uuid.FromString(identifRole)
-	role.UserID, err = uuid.FromString(identifUser)
-	role.RepoID, err = uuid.FromString(identifReposit)
+	err = idrole.UnmarshalText([]byte(identifRole))
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error UnmarshalText sql request ", err) //
+		return role, err
+	}
+	err = iduser.UnmarshalText([]byte(identifUser))
+	if err != nil {
+		log.Errorf("Error UnmarshalText sql request ", err) //
+		return role, err
+	}
+	err = idrepo.UnmarshalText([]byte(identifReposit))
+	if err != nil {
+		log.Errorf("Error GetAccessRoles scan ", identifRole, err)
 		return role, err
 	}
 	logrus.Trace("get info about Role ", role)
@@ -331,18 +418,31 @@ func (d *DB) GetAccessRoles() ([]AccessRole, error) {
 	var roles []AccessRole
 	row, err := d.db.Query("SELECT * FORM roles ")
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error GetAccessRoles ", err)
 		return roles, err
 	}
 	for row.Next() {
+		var idrole, iduser, idrepo IDT
 		var identifRole, identifUser, identifReposit string
 		var role AccessRole
 		row.Scan(&identifRole, &identifUser, &identifReposit, &role.Branches, &role.Created, &role.Deleted)
-		role.RoleID, err = uuid.FromString(identifRole)
-		role.UserID, err = uuid.FromString(identifUser)
-		role.RepoID, err = uuid.FromString(identifReposit)
+		err = idrole.UnmarshalText([]byte(identifRole))
 		if err != nil {
-			log.Errorf("Error %s", err)
+			log.Errorf("Error UnmarshalText sql request ", err) //
+			return roles, err
+		}
+		err = iduser.UnmarshalText([]byte(identifUser))
+		if err != nil {
+			log.Errorf("Error UnmarshalText sql request ", err) //
+			return roles, err
+		}
+		err = idrepo.UnmarshalText([]byte(identifReposit))
+		if err != nil {
+			log.Errorf("Error UnmarshalText sql request ", err) //
+			return roles, err
+		}
+		if err != nil {
+			log.Errorf("Error GetAccessRoles scan ", identifRole, err)
 			return roles, err
 		}
 		logrus.Trace("get info about Role ", role)
@@ -351,31 +451,39 @@ func (d *DB) GetAccessRoles() ([]AccessRole, error) {
 	return roles, nil
 }
 
-func (d *DB) UserByKey(key []byte) (*User, error) {
-	err := d.init()
+func (d *DB) UserByKey(key []byte) (User, error) {
+	var user User
+	var err error
+	row := d.db.QueryRow("SELECT userid FROM keys WHERE key = ?", key) // что делает и что выводит ?
 	if err != nil {
-		log.Errorf("Error %s", err)
-		return nil, err
+		log.Errorf("Error UserByKey ", key, err)
+		return user, err
 	}
-	// что делает и что выводит ?
-	return nil, nil
+	var answer string
+	err = row.Scan(&answer)
+	if err != nil {
+		log.Errorf("Error Scan sql request ", key, err)
+		return user, err
+	}
+	row = d.db.QueryRow("SELECT * FROM users WHERE userid = ?", answer)
+	err = row.Scan(&answer, &user.Name, &user.Email, &user.Created, &user.Edited, &user.Deleted)
+	if err != nil {
+		log.Errorf("Error Scan sql request ", key, err)
+		return user, err
+	}
+	return user, nil
 }
 
 func (d *DB) CheckPermissions(userid IDT, repoid IDT, branch string) (bool, error) {
-	err := d.init()
-	if err != nil {
-		log.Errorf("Error %s", err)
-		return false, err
-	}
 	row, err := d.db.Query("SELECT roleid FROM roles WHERE userid = ? AND WHERE repoid = ? AND WHERE branch = ?", userid.String(), repoid.String(), branch)
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error CheckPermissions ", userid, repoid, branch, err)
 		return false, err
 	}
 	var roleid string
 	err = row.Scan(&roleid)
 	if err != nil {
-		log.Errorf("Error %s", err)
+		log.Errorf("Error CheckPermissions scan", userid, repoid, branch, roleid, err)
 		return false, err
 	}
 	return true, err
